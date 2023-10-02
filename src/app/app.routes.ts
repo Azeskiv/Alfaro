7bb6416139886a574c2df85866ccb9496c8a4a70
Azeskiv <130972262+Azeskiv@users.noreply.github.com>
2023-10-02 13:16:21 +0200

    primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+  },
+  {
+    path: 'datos',
+    loadComponent: () => import('./datos/datos.page').then( m => m.DatosPage)
+  },
+];
